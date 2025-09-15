bf32b9795c8a5576e5cd11382ff10b2b60c36736
Parvati-Mutalik <parvatimutalik2003@gmail.com>
2025-09-15 08:38:46 +0530

    Update HomePage1.js

--- src/pages/HomePage1.js
+++ src/pages/HomePage1.js
@@ -247,7 +247,7 @@ function HomePage1() {
         <nav className="nav">
           <div className="logoSection">
             <div className="logo">
-             <img src="./Sevauk logo.jpg" alt="Sevauk logo" />
+             <img alt="Sevauk logo" src="/Sevauk logo.jpg">
             </div>
             <div className="logo">
               <img src="/logo22.jpg" alt="Arunodaya Logo" />
